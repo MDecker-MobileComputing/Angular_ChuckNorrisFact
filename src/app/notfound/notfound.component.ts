2ce2afd44bdeabb52acc3cf2d96a50d7414a16dd
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-04-11 00:04:22 +0200

    Update notfound.component.ts

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
+/**
+ * Custom component to be shown when component requested from router is not found.
+ * When served via web server then 404 page of sever will be shown instead?
+ */
 @Component({
   selector: 'mide-notfound',
   templateUrl: './notfound.component.html',
